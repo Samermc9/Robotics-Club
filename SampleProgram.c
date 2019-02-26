5a07e16d85c9c2616bb8766de91caf60194e139f
Samer Saboba <samermc9@gmail.com>
2019-02-26 07:34:27 +0300

    Update SampleProgram.c

--- SampleProgram.c
+++ SampleProgram.c
@@ -33,6 +33,5 @@ task main() //The program starts here, everything inside will be executed
 	
 	
 	motor[motor1] = 50; //Set the leftMotor (motor1) to half power (50)
-	motor[motor6] = -50; //Set the rightMotor (motor6) to half power (50)
 	sleep(2000); //Wait for 2 seconds before continuing on in the program.
 }
